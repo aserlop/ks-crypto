00ceae1d3a28bd2f1f4b275afa9fa1297b6bcb03
aserlop <alvaro.serrano@cunef.edu>
2020-08-03 21:05:28 +0200

    after continuously failing trying to create the environment as a initialization action of the cluster

--- bin/dataproc_ini/create_env.sh
+++ bin/dataproc_ini/create_env.sh
@@ -5,9 +5,9 @@ KERNEL_NAME="k_p_ks_crypto"
 
 conda create -n $ENV_NAME python=3.7
 conda activate $ENV_NAME
-python /home/ks-crypto/setup.py clean
-pip install /home/ks-crypto/setup.py
+cd /home/ks-crypto
+python setup.py clean
+pip install .
 python -m ipykernel install --user --name $ENV_NAME
 sudo mv /root/.local/share/jupyter/kernels/${ENV_NAME}/ /opt/conda/anaconda/share/jupyter/kernels/
 conda deactivate
-cd ~
