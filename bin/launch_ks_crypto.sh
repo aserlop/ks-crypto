e7aa91da9c94d5b24693985f2c75475d429beecb
aserlop <alvaro.serrano@cunef.edu>
2020-08-18 22:44:57 +0200

    launch whole period

--- bin/launch_ks_crypto.sh
+++ bin/launch_ks_crypto.sh
@@ -12,7 +12,7 @@ REGION=europe-west2
 
 # dates
 DATE_END='2017-11-01'
-NUM_PERIODS=22
+NUM_PERIODS=22 # total 22
 PERIOD_UNIT='months'
 
 
@@ -21,7 +21,7 @@ PERIOD_UNIT='months'
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Create a dataproc
-# ./00_start_dataproc.sh ${CLUSTER_NAME} ${REGION} ${BUCKET_NAME} # && \
+./00_start_dataproc.sh ${CLUSTER_NAME} ${REGION} ${BUCKET_NAME} && \
 
 ./01_extract_data.sh ${CLUSTER_NAME} ${REGION} ${BUCKET_NAME} ${DATE_END} ${NUM_PERIODS} ${PERIOD_UNIT}
 
